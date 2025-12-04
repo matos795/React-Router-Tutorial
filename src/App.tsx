e4bd57cad31bcc56b00bf15f4e2d796325251406
matos795 <matosalexandre795@gmail.com>
2025-12-04 11:37:15 -0300

    Nested routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,10 @@
 import './App.css'
-import { Link } from 'react-router-dom'
+import { Link, Outlet } from 'react-router-dom'
 
 function App() {
   return(
-    <nav
+    <>
+      <nav
         style={{
           borderBottom: "solid 1px",
           paddingBottom: "1rem",
@@ -12,6 +13,9 @@ function App() {
         <Link to="/invoices">Invoices</Link> |{" "}
         <Link to="/expenses">Expenses</Link>
       </nav>
+      
+      <Outlet />
+    </>
   );
 }
 
