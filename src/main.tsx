9634236162842fee0ba5b8997d51cbe71d594a68
matos795 <matosalexandre795@gmail.com>
2025-12-04 14:34:32 -0300

    Index Routes

--- src/main.tsx
+++ src/main.tsx
@@ -5,21 +5,25 @@ import Expenses from './routes/Expenses/index.tsx';
 import Invoices from './routes/Invoices/index.tsx';
 import NotFound from './routes/NotFound/index.tsx';
 import Invoice from './routes/Invoices/Invoice/index.tsx';
+import InvoicesIndex from './routes/Invoices/InvoicesIndex/index.tsx';
+import HomeIndex from './routes/HomeIndex/index.tsx';
 
 createRoot(document.getElementById('root')!).render(
-    <BrowserRouter>
-      <Routes>
-        <Route path="/" element={<App />}>
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<App />}>
+        <Route index element={<HomeIndex />} />
 
-          <Route path="expenses" element={<Expenses />} />
+        <Route path="expenses" element={<Expenses />} />
 
-          <Route path="invoices" element={<Invoices />}>
-            <Route path=":invoiceId" element={<Invoice />} />
-          </Route>
-          
-          <Route path="*" element={<NotFound />} />
-
+        <Route path="invoices" element={<Invoices />}>
+          <Route index element={<InvoicesIndex />} />
+          <Route path=":invoiceId" element={<Invoice />} />
         </Route>
-      </Routes>
-    </BrowserRouter>,
+
+        <Route path="*" element={<NotFound />} />
+
+      </Route>
+    </Routes>
+  </BrowserRouter>,
 )
---
+++ src/routes/HomeIndex/index.tsx
@@ -0,0 +1,7 @@
+export default function HomeIndex() {
+    return(
+        <main style={{ padding: "1rem" }}>
+            <p>Bem Vindo!</p>
+        </main>
+    );
+}
