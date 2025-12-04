83f65175a437fccc5f88ef2754a0972f7e922a4a
matos795 <matosalexandre795@gmail.com>
2025-12-04 14:50:53 -0300

    Active Links

--- src/routes/Invoices/index.tsx
+++ src/routes/Invoices/index.tsx
@@ -1,5 +1,6 @@
+import './styles.css';
 import { getInvoices } from "../../data";
-import { Link, Outlet } from "react-router-dom";
+import { NavLink, Outlet } from "react-router-dom";
 
 export default function Invoices() {
 
@@ -14,13 +15,13 @@ export default function Invoices() {
         }}
       >
         {invoices.map((invoice) => (
-          <Link
-            style={{ display: "block", margin: "1rem 0" }}
+          <NavLink
+            className={({ isActive }) => isActive ? "dblock nav-red" : "dblock nav-blue"}
             to={`/invoices/${invoice.number}`}
             key={invoice.number}
           >
             {invoice.name}
-          </Link>
+          </NavLink>
         ))}
       </nav>
 
